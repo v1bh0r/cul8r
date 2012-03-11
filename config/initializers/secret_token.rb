d3a97c13a54922fcbcdba4110fccea4787655953
Vibhor Mahajan <Vibhor.Mahajan@gmail.com>
2012-03-11 23:25:48 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cul8r::Application.config.secret_token = '91c6fe90a660f6b65b310c8c74ee8c0326424c20136e7d515d7c30987c5d94a8cd02fd018d135077f7b79b4c6586452ee85d9f6acba4ca7b375046463c966159'
