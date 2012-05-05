15578abd53162f6f759a794fb815642c939897ac
Vibhor Mahajan <Vibhor.Mahajan@gmail.com>
2012-05-05 11:01:07 +0530

    Remove url rewrite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,6 @@ Cul8r::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-               gem 'rack-rewrite'
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
