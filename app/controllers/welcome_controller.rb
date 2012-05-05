a51d35766c12e8d76452ca4bd59b03946e6409ad
Vibhor Mahajan <Vibhor.Mahajan@gmail.com>
2012-05-05 11:10:59 +0530

    Remove caching on index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
   before_filter :goto_main_page_if_logged_in
 
-  caches_page :index
-  
   def index
   end
   
