6bb2c0b0ba82f66f7b3e5f076591d4490e54aeeb
Vibhor Mahajan <Vibhor.Mahajan@gmail.com>
2012-05-05 10:53:17 +0530

    Added some caching mechanism

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Cul8r::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+               gem 'rack-rewrite'
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
