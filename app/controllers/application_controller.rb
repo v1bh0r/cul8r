80cfbbeef0fbe97d12627ac91b74a585137fafe4
vibhor86 <vibhor.mahajan@gmail.com>
2012-05-22 01:37:20 +0530

    Fix naked url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
   helper :layout
+  before_filter :fix_naked_domains
+
+  def fix_naked_domains
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
 end
