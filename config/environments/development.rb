28c14717e64dd0dddf4e1d7e126415614703ff88
vibhor86 <vibhor.mahajan@gmail.com>
2012-05-22 01:38:50 +0530

    Fix naked urls

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,4 +45,5 @@ Cul8r::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  APP_DOMAIN = 'localhost:3000'
 end
