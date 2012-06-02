a83257507c580f5da586ea0447dcd35670e47ec0
Vibhor Mahajan <Vibhor.Mahajan@gmail.com>
2012-06-02 18:55:07 +0530

    Added featured page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,25 @@ class User < ActiveRecord::Base
   after_create :welcome_email
 
   has_many :watch_list_movie_entries
-  
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,:admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin
 
   def welcome_email
     UserNotification.welcome(self).deliver
   end
-  
+
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token.extra.raw_info
     if user = User.where(:email => data.email).first
       user
     else # Create a user with a stub password. 
-      User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+      User.create!(:email => data.email, :password => Devise.friendly_token[0, 20])
     end
   end
 
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
   end
 
   def movies_in_watch_list
-    self.watch_list_movie_entries.all(:include=>:movie, :order => "watch_list_movie_entries.id asc").collect{|entry| entry.movie}
+    self.watch_list_movie_entries.all(:include => :movie, :order => "watch_list_movie_entries.id asc").collect { |entry| entry.movie }
   end
 
   def role?
@@ -47,5 +47,9 @@ class User < ActiveRecord::Base
 
   def admin?
     self.admin
+  end
+
+  def recommended_movies
+    Movie.all( :conditions => "critics_score > 70", :limit => 6, :order=>"release_dates desc")
   end
 end
